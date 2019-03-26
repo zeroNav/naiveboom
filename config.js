fb797256e848b4873ae12c3ed36c7103d3c74c38
碎碎念ZWS <nooldey@users.noreply.github.com>
2019-03-26 13:44:51 +0800

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 module.exports = {
 	// redis服务器配置
 	redis_host: '127.0.0.1',
-	redis_port: '6379',
+	redis_port: process.env.PORT,
 	redis_pwd: null, // 没有redis密码则写 null
 	// redis Hash列表名
 	field_lists: 'naives',
